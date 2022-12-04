4c0d57e4efaa44bcec4c4a1a25a8c01d2709333a
Jenn-Torres <jennifer.rosalia.gibert@gmail.com>
2022-12-04 00:01:38 +0000

    removed hardcoded values

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@ let apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
+function formatDate (timesTamp) {
+    let date = new Date(timesTamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -19,6 +37,11 @@ function displayTemperature(response) {
 
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
+
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+
+
 
 axios.get(apiUrl).then(displayTemperature);
